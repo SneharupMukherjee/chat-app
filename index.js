c5afcf68b764b8ddad361fc74f357b3a4bef1414
psyched <student.sneharup.m@snuniv.ac.in>
2022-01-06 14:53:15 +0530

    final

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
+const { Server } = require('socket.io');
 const express = require('express');
 const http = require('http');
 
 const app = express();
 const server = http.createServer(app);
+const io = new Server(server);
 
 app.get('/', (req, res) => {
-    res.send(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('an user connected');
+    
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
 });
 
 server.listen(3000, () =>  {
     console.log('listening on *:3000')
-})
+});
